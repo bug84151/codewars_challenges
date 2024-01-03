b1714a2bd864deac73db0d9dce2c7c308fb87f44
bug84151 <bug84151@gmail.com>
2024-01-03 21:59:09 +0100

    persistence second commit

--- 8kyu/persistence.js
+++ 8kyu/persistence.js
@@ -1,14 +1,14 @@
-let num = 9
-let numStr = num.toString()
-let sumOfProducts = 1
-let counter = 0
+function persistence(num) {
+  let numStr = num.toString();
+  let counter = 0;
 
-while (numStr.length > 1) {
+  while (numStr.length > 1) {
+    let sumOfProducts = 1;
     for (let i = 0; i < numStr.length; i++) {
-      sumOfProducts = sumOfProducts * numStr[i];
-      console.log(sumOfProducts);
-      counter++;
-      console.log(counter);
+      sumOfProducts = sumOfProducts * Number(numStr[i]);
     }
-    numStr = sumOfProducts.toString()
+    numStr = sumOfProducts.toString();
+    counter = counter + 1;
+  }
+  return counter;
 }
