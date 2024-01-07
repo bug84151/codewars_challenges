0a864bd277c31d543f02cdeeb50f44a3985d42a5
bug84151 <bug84151@gmail.com>
2024-01-07 22:35:00 +0100

    you're a square challenge third commit

--- 7kyu/your_a_square.js
+++ 7kyu/your_a_square.js
@@ -4,7 +4,7 @@ var isSquare = function (n) {
   } else if (n < 0) {
     return false;
   }
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i <= n; i++) {
     if (i ** 2 == n) {
       return true;
     }
