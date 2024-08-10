59a45f3fc23cea7c62e5aa043668c7ce2cbcf44e
bug84151 <bug84151@gmail.com>
2024-08-10 07:33:00 +0100

    sum of two lowest positive integers

---
+++ 7kyu/sumOfLowestPositiveNum.js
@@ -0,0 +1,9 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+let numbers = [10, 343445353, 3453445, 3453545353453];
+let sum;
+let count;
+for (let i = 0; numbers.length > i; i++) {
+    for (let j = 0; numbers.length > j; j++) {
+            console.log(numbers[i],numbers[j]);
+  }
+}
